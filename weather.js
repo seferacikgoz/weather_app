02ffdb014849ba5fc0053b3f40f649ed26002ada
sefer <seferackgz@gmail.com>
2022-10-04 16:21:47 +0300

    v3

--- weather.js
+++ weather.js
@@ -3,24 +3,23 @@ const input = document.querySelector(".container input")
 const msg = document.querySelector("span.msg")
 const list = document.querySelector(".ajax-section .cities")
 
-localStorage.setItem("tokenKey", "RAPAIooyOVFdRNn7gPi6i8vUp3OJvy0Np5wgMGgNO0a2a258kya95/arqJmhPrWc");
+localStorage.setItem("tokenKey", "NhICtBcwPwtiuY2s0FjIfyL1bWQXw06+pDkgPjNnMdMuuxOs1d1uWtP5qlDBLQxj")
+/* localStorage.setItem("tokenKeyEncrypted", EncryptStringAES("39a65e65af90f699e86e054d41b09249"))
+ */
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    getWeatherDataFromApi()
+})
 
-form.addEventListener("submit", (event)=>{
-    event.preventDefault();
-    getWeatherDataFromApi();
-});
+//* Get api func. (http methods == Verbs)
+const getWeatherDataFromApi = async() => {
+    /* alert("http request is gone") */
+    const tokenKey = DecryptStringAES("NhICtBcwPwtiuY2s0FjIfyL1bWQXw06+pDkgPjNnMdMuuxOs1d1uWtP5qlDBLQxj")
+    /* alert(tokenKey) */
+    const inputValue = input.value
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}`
 
-//Get api func. (http methods == Verbs)
-const getWeatherDataFromApi = async() =>{
-    //alert("http request is gone!");
-    const tokenKey = DecryptStringAES(localStorage.getItem("tokenKey"));
-    //alert(tokenKey);
-    const inputValue = input.value;
-    const units = "metric";
-    const lang = "tr"; 
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`;
-
-    const response = await fetch(url).then(response=>response.json());
+    const response = await fetch(url).then(response => response.json())
     console.log(response);
 
 }
