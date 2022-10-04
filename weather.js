9791feeeced466e9f6ab469ff5efe79b802a5efc
sefer <seferackgz@gmail.com>
2022-10-04 16:44:25 +0300

    v4

--- weather.js
+++ weather.js
@@ -17,9 +17,30 @@ const getWeatherDataFromApi = async() => {
     const tokenKey = DecryptStringAES("NhICtBcwPwtiuY2s0FjIfyL1bWQXw06+pDkgPjNnMdMuuxOs1d1uWtP5qlDBLQxj")
     /* alert(tokenKey) */
     const inputValue = input.value
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}`
+    const units = "metric"
+    const lang = "tr"
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${tokenKey}&units=${units}&lang=${lang}`
 
     const response = await fetch(url).then(response => response.json())
     console.log(response);
+    const { main, sys, weather, name} = response
+
+
+    const iconUrl = `http://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
+
+    const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+
+    const createdLi = document.createElement("li")
+    createdLi.classList.add("city")
+    createdLi.innerHTML = `<h2 class="city-name" data-name="${name}, ${sys.country}">
+    <span>${name}</span>
+    <sup>${sys.country}</sup>
+</h2>
+<div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+<figure>
+    <img class="city-icon" src="${iconUrl}">
+    <figcaption>${weather[0].description}</figcaption>
+</figure>`;
+
 
 }
